d23a9f726835c02d57f8a5300647b9c4528d8789
Alejandro Mansor <amansor@gmail.com>
2018-05-29 08:33:07 -0300

    implementacion redux-form. creacion formulario

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -1,12 +1,66 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostNew extends Component{
-render(){
-  return(
-    <div> POST NEW COMPONENT TESTING GIT BRANCH 6! (BRANCH 2 desde batch) </div>
-  )
-}
+  
+  renderField(field){
+    return(
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+  
+  render(){ 
+    return(
+      <form> 
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field 
+          label="Post content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    )
+  }
 
 }
 
-export default PostNew;
+function validate(values){
+  //values es un OBJETO que mantiene los valores de los Fields por cada name, ejemplo: {title:'titulo', categories:'categorias!'}
+  const errors = {};
+
+  //validaciones
+  if (!values.title || values.title.length < 3){
+    errors.title = "Enter a title with at least 3 characters!";
+  }
+  if (!values.categories){
+    errors.categories="Enter some categories!";
+  }
+  if (!values.content){
+    errors.content = "Enter some content!";
+  }
+
+  //si errors is empty --> form OK to submit (no errors)
+  return errors;
+
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostNew);
